6aaea769f5c491cfd2817e4db46a00bc9b53a27c
vidhanshu <vidhanshu.borade.cs@ghrce.raisoni.net>
2023-03-20 23:32:05 +0530

    latest commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,12 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
-})
-    .then((res) => {
-        console.log("Database connected!")
-    })
-    .catch((err) => {
-        console.log("Error connecting!")
-    })
+  })
+  .then((res) => {
+    console.log("Database connected!");
+  })
+  .catch((err) => {
+    console.log("Error connecting!");
+  });
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,24 +1,26 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/user")
+const User = require("../models/user");
 
 const auth = async (req, res, next) => {
-    try {
-        const accessToken = req.headers.authorization?.replace("Bearer ", "");
-        const decoded = jwt.verify(accessToken, process.env.SECRETE_KEY);
-        console.log(decoded)
-        const user = await User.findById(decoded._id);
+  try {
+    const accessToken = req.headers.authorization?.replace("Bearer ", "");
+    const decoded = jwt.verify(accessToken, process.env.SECRETE_KEY);
+    const user = await User.findById(decoded._id);
 
-        //either user doesn't exists or the token in the user doesn't exists
-        if (!user || (user && !user.accessTokens.some(({ token }) => token === accessToken))) {
-            return res.status(401).send({ error: "please do authenticate" })
-        }
-
-        req.user = user;
-        req.token = accessToken;
-        next()
-    } catch (err) {
-        res.status(500).send(err);
+    //either user doesn't exists or the token in the user doesn't exists
+    if (
+      !user ||
+      (user && !user.accessTokens.some(({ token }) => token === accessToken))
+    ) {
+      return res.status(401).send({ error: "please do authenticate" });
     }
-}
+
+    req.user = user;
+    req.token = accessToken;
+    next();
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
 
 module.exports = auth;
